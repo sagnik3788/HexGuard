a0bae44142f99bab0d6215a287a21e6127c673bb
sagnik3788 <116512372+sagnik3788@users.noreply.github.com>
2024-10-01 20:06:55 +0530

    add

--- saas/web/hexguard/src/app/page.tsx
+++ saas/web/hexguard/src/app/page.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import Landing from './components/landing-page';
-// import { Button } from '@/components/ui/button';
-// import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-// import Image from 'next/image';
 
 export default function LandingPage() {
   return (
